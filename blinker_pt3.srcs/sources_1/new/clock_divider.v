b16ed637b9acbea91de3daacc2de66e1ef9758e8
Ashvin Verma <ashvin.vermarocks@gmail.com>
2024-05-27 18:54:18 +0530

    initial commit

---
+++ blinker_pt3.srcs/sources_1/new/clock_divider.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 26.05.2024 10:57:18
+// Design Name: 
+// Module Name: clock_divider
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module clock_divider(
+    input clock,
+    input [4:0] factor,
+    output clock
+    );
+endmodule
