abdcc383d0bf881aecb946b863d712789ea13f18
Ashvin Verma <ashvin.vermarocks@gmail.com>
2024-05-27 19:41:34 +0530

    clock_divider.v implemented

--- blinker_pt3.srcs/sources_1/new/clock_divider.v
+++ blinker_pt3.srcs/sources_1/new/clock_divider.v
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
+// Company: ash
+// Engineer: Ash
 // 
 // Create Date: 26.05.2024 10:57:18
 // Design Name: 
@@ -21,8 +21,18 @@
 
 
 module clock_divider(
-    input clock,
-    input [4:0] factor,
-    output clock
+    input CLK_I,
+    output CLK_O
     );
+    reg[27:0] cnt=28'd0;
+    parameter DIVISOR = 28'd2;
+
+    always @(posedge CLK_I)
+    begin
+        cnt <= cnt + 28'd1;   
+        if(cnt >= ( DIVISOR-1 ) ) 
+            cnt <= 28'd0;       
+        CLK_O <= ( cnt < DIVISOR >> 1 )? 1'b1 : 1'b0;  
+    end
+
 endmodule
